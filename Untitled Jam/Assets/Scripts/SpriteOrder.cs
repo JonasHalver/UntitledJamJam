a3892b3e46d0075bc240b2b8414be5fa97ae0ad1
jesperqwop <jespertuan@hotmail.com>
2019-10-31 10:54:40 +0100

    asd

---
+++ Untitled Jam/Assets/Scripts/SpriteOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOrder : MonoBehaviour
+{
+    SpriteRenderer SR;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SR.sortingOrder = (int) (-transform.position.y * 10);
+    }
+}
