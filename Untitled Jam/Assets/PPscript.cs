b779077fbc97dd4b7b648198124566e1e19b9016
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-11-01 12:25:42 +0100

    post

---
+++ Untitled Jam/Assets/PPscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPscript : MonoBehaviour
+{
+    ColorGrading cg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PostProcessVolume volume = gameObject.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings<ColorGrading>(out cg);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cg.saturation.value = PlayerHealth.health - 100;
+    }
+}
